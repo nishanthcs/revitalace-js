95ba6800774739d9289e40b19deb71e5dbcce48d
nishanthcs <nishanth.menon@gmail.com>
2021-03-27 01:26:21 -0700

    feat: update demo for tooltip callback feature

--- sample.js
+++ sample.js
@@ -48,6 +48,16 @@ editor.setOptions({
 		"m : 'JS has -infinity and infinity'\n" +
 		"}",
 		defs : defObject,
+		/**
+		 * This is a function will be called for every completion. Add or remove
+		 * tern completions in this callback and return.
+		 *
+		 * The below params are injected by revitalace.
+		 * @param editorSession - Ace edit session object
+		 * @param currentLine - Current line where the completion is triggered
+		 * @param completions - Array of completions resolved by tern
+		 * @return completions - Array of completions updated in this callback
+		 */
 		completionsCallback: (editorSession, currentLine, completions) => {
 			console.log('Completion Line' + currentLine);
 			console.log(JSON.stringify(completions));
@@ -62,6 +72,30 @@ editor.setOptions({
 				});
 
 			return completions;
+		},
+		/**
+		 * This is a function will be called for every tool tip render. modify result and return.
+		 *
+		 * In this example, every doc element will be replaced with text 'Where is Waldo' :o)
+		 *
+		 * The below params are injected by revitalace.
+		 * @param editorSession - Ace edit session object
+		 * @param currentToken - Current token ace object where the completion is triggered
+		 * @param toolTipInfo - Result object compiled by revitalace
+		 * @return toolTipInfo - Final result that would be used to render the tooltip
+		 */
+		tooltipCallBack: (editorSession, currentToken, toolTipInfo) => {
+			console.log('Tooltip Token' + currentToken.value);
+			console.log(JSON.stringify(toolTipInfo));
+			// Update the doc here.
+			const customizedToolTipInfo = {
+				type: "fn(input: ?)",
+				name: "getTooltipInfo",
+				doc: "*Dynamically customized in sample.js*\n"+toolTipInfo.doc,
+				def: toolTipInfo.def,
+				refs: toolTipInfo.refs
+			}
+			return customizedToolTipInfo;
 		},
 		options: {
 			useEcmaDefs: true,
